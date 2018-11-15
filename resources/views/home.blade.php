c2908ad8f367cb76d16c9c8bb2bbfae8acf30abf
ZZC\wenwenbin <382899012@qq.com>
2018-11-15 17:07:22 +0800

    首页

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,43 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>欢迎来到laravel5.1</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        {{--<link href="{{URL::asset('css/home.css')}}" rel="stylesheet" type="text/css">--}}
+        <link href="<?php echo asset('css/home.css');?>" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
-            <a href="/login">登录</a>
+            <div class="banner">
+                <div class="banner-left">
+                    <p>欢迎来到laravel5.1</p>
+                </div>
+                <div class="banner-right">
+                    <ul>
+                        <li><a href="<?php echo url('login')?>">登录</a></li>
+                        <li><a href="<?php echo url('login')?>">注册</a></li>
+                    </ul>
+
+                </div>
+
+            </div>
+
         </div>
     </body>
 </html>
