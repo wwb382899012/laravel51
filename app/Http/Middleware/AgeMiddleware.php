2a7bd4f1d9884a9d1dc0d13c8e519ec7797241e5
ZZC\wenwenbin <382899012@qq.com>
2018-11-16 11:22:05 +0800

    路由分组 - 中间件

---
+++ app/Http/Middleware/AgeMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AgeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($request->input('age')<18)
+            return redirect()->route('refuse');   //年龄<18岁禁止访问
+        return $next($request);
+    }
+}
